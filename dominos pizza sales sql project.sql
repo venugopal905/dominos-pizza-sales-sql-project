cbbab0dfffa65df61d2decee6536c315403a785b
venu gopal <talisettyvenugopal@gmail.com>
2026-01-07 21:16:54 +0530

    Add files via upload

---
+++ dominos pizza sales sql project.sql
@@ -0,0 +1,5 @@
+-- Calculate the total revenue generated from pizza sales.
+select 
+round(sum(order_details.quantity * pizzas.price),2) as totalprice 
+from order_details join pizzas
+on pizzas.pizza_id = order_details.pizza_id
